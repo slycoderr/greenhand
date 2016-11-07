f3c8a52f8b13b563ecb4c64077f1ec745bd35e95
adkerti <adkerti@ADKERTI>
2016-11-07 13:07:11 -0500

    initial

---
+++ GreenServer/GreenHandClient/GreenHandClient/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace GreenHandClient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new GreenHandClient.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
